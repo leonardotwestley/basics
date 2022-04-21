81e744234f2cd1ae628e1430a8a3e480fc9dda93
Leonardo Westley <leonardowestley@Leonardos-MacBook-Air.local>
2022-04-21 11:57:06 -0400

    chicken or the egg

---
+++ async.js
@@ -0,0 +1,10 @@
+console.log('This comes first. 🥚')
+
+setTimeout(function () {
+    console.log('This is in the middle...')
+}, 0)
+
+console.log('This is in the middle...')
+
+console.log('This is the end. 🐓')
+
